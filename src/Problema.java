f463f552f608d0f24806ad78f67debd65135166e
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-28 14:09:29 -0300

    Relatorio (apresenta erro)

--- src/Problema.java
+++ src/Problema.java
@@ -8,11 +8,21 @@ public class Problema {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		String nome;
+		String nome, alturamaior;
 		char sexo, outroatleta;
-		double altura, peso;
+		double altura, peso, pesomedio, pesototal,alturaatleta, porcentagemhomem, alturamediamulher, alturatotalmulher;
+		int contagem, numeromulher, numerohomem;
 
 		outroatleta = 0;
+		contagem = 0;
+		pesototal = 0;
+		altura = 0;
+		numerohomem = 0;
+		numeromulher = 0;
+		alturamediamulher = 0;
+		alturatotalmulher = 0;
+		alturaatleta = 0;
+		
 		
 		do {
 			System.out.print("Nome: ");
@@ -20,19 +30,33 @@ public class Problema {
 				sc.nextLine();
 			}
 			nome = sc.nextLine();
+			alturamaior = nome;
+			alturaatleta = altura;
 			System.out.print("Sexo: ");
 			sexo = sc.next().charAt(0);
 			while (sexo != 'F' && sexo != 'M') {
 				System.out.print("Valor Inválido! Digite novamente: ");
 				sexo = sc.next().charAt(0);
 			}
+			if (sexo == 'M') {
+				numerohomem = numerohomem + 1;
+			}
 			
 			System.out.print("Altura: ");
 			altura = sc.nextDouble();
+			if (sexo == 'F') {
+				numeromulher = numeromulher + 1;
+				alturatotalmulher = alturatotalmulher + altura;
+			}
 			while (altura < 0) {
 				System.out.print("O valor da altura não pode ser negativo! Digite novamente: ");
 				altura = sc.nextDouble();
 			}
+			alturaatleta = altura;
+			while (alturaatleta > altura) {
+			    alturamaior = nome;
+			}
+
 			
 			System.out.print("Peso: ");
 			peso = sc.nextDouble();
@@ -42,8 +66,27 @@ public class Problema {
 			}
 			System.out.print("Digitar outro atleta (S/N)? ");
 			outroatleta = sc.next().charAt(0);
+			
+			contagem = contagem + 1;
+			pesototal = pesototal + peso;
+			
 
 		} while (outroatleta == 'S' || outroatleta == 's');
+		
+		pesomedio = pesototal / contagem;
+		porcentagemhomem = (100.0 / contagem) * numerohomem;
+		alturamediamulher = alturatotalmulher / numeromulher;
+		
+		
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Peso médio dos atletas: %.2f%n" , pesomedio);
+		System.out.println("Atleta mais alto: " + alturamaior);
+		System.out.printf("Porcentagem de homens: %.1f %%%n", porcentagemhomem );
+		System.out.printf("Altura média das mulheres: %.2f%n" , alturamediamulher);
+		System.out.println();
+		System.out.print("FIM DO PROGRAMA!");
 
 		sc.close();
 	}
